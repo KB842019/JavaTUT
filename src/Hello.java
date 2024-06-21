638638ec69b77c95ff84373a38237767ce58667c
KB842019 <kb842019@gmail.com>
2024-06-21 08:13:11 +0530

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,22 @@
+public class Hello {
+    static int sum(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.print("Hello World ");
+        System.out.print("Hello World");
+        System.out.println("Hello World");
+        int number=20;
+        System.out.println("value of number is "+number);
+//        System.out.println(args[0]);
+        int result=sum(4,6);
+        System.out.println(result);
+
+
+        result == 10 ?
+
+
+    }
+}
+
